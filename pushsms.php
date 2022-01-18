0e6b53f906359fb8ab32935fa811981a911e4bc5
makaveli45 <richard.oduro45@gmail.com>
2022-01-18 22:11:40 +0000

    pushsms
    it has php mailer and hubtel sms example

--- pushsms.php
+++ pushsms.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once("hubtel/Demo.php");
 try {
 				$messageResponse = $messagingApi->sendQuickMessage("Mo-Crane", "0541778545", "Hello, This is a test");
 
@@ -15,4 +15,29 @@ try {
 			} catch (Exception $ex) {
 		echo $ex->getTraceAsString();
 			}
+			?>
+			
+			
+			
+			<?php
+			//for email just use mail(to;subject,message,headers);
+			
+			
+			//eg.
+			//PHP MAILER
+/******************************************************************************************
+    $from = "info@richardoduro.com";
+    $to = "chrappah2006@gmail.com";
+    $subject = "$username Made a Transfer";
+    $message = "$username made a transfer($items) from $from warehouse to $to";
+    $headers = "From:" . $from;
+    if(mail($to,$subject,$message, $headers)) {
+	//	echo "The email message was sent.";
+    } else {
+    //	echo "The email message was not sent.";
+    }
+************************************************************************/
+    
+				//PHP MAILER
+			
 			?>
